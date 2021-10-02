c58d8423b503330cc29ede21655a3e750924d993
David Ortinau <david.ortinau@microsoft.com>
2021-10-02 13:06:11 -0500

    WIP moving files around

---
+++ Pairs/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using Pairs.ViewModels;
+
+namespace Pairs
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void MainPageViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == nameof(MainPageViewModel.State) &&
+			    ((MainPageViewModel)BindingContext).State == Models.LevelState.Complete)
+			{
+				//TrophyAnimation.PlayAnimation();
+			}
+		}
+	}
+}
